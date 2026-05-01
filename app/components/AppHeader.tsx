f3bf6b9ca67dcf85cf321bf00285cc64c82a95df
Assih Atcholi <markatcholi918@gmail.com>
2026-04-30 22:23:37 -0500

    Add OOS/OOT link to AppHeader component

--- app/components/AppHeader.tsx
+++ app/components/AppHeader.tsx
@@ -53,6 +53,7 @@ export default function AppHeader() {
         <a href="/ncmrs" style={{ marginRight: "12px" }}>NCMRs</a>
         <a href="/capa" style={{ marginRight: "12px" }}>CAPA</a>
         <a href="/audits" style={{ marginRight: "12px" }}>Audits</a>
+        <a href="/oos-oot" style={{ marginRight: "12px" }}>OOS/OOT</a>
         <a href="/audit" style={{ marginRight: "12px" }}>Audit Trail</a>
         <a href="/admin/master-data">Admin Master Data</a>
       </nav>
