fbb99cc92f3b83a0e9fd11f262de9660e56e50ac
Assih Atcholi <markatcholi918@gmail.com>
2026-05-13 23:02:16 -0500

    Update page.tsx

--- app/suppliers/[id]/page.tsx
+++ app/suppliers/[id]/page.tsx
@@ -367,8 +367,8 @@ export default function SupplierProfilePage() {
           <Link href="/suppliers" style={{ marginRight: "12px" }}>Supplier List</Link>
           <Link href="/supplier-quality-dashboard" style={{ marginRight: "12px" }}>Supplier Dashboard</Link>
           <Link href={`/supplier-quality/scorecards/${supplier.id}`} style={{ marginRight: "12px" }}>Supplier Scorecard</Link>
-          <Link href={`/suppliers/${supplier.id}/audits`}>Supplier Audits</Link> 
-          <Link href={`/suppliers/${supplier.id}/documents`}> Supplier Documents </Link>   
+           <Link href={`/suppliers/${supplier.id}/documents`}> Supplier Documents </Link>  
+          <Link href={`/suppliers/${supplier.id}/audits`}>Supplier Audits</Link>  
         </div>
         
       </div>
