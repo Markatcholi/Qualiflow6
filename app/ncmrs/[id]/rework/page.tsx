f6429cff257bf21e29d1bfac8af609357c97e082
Assih Atcholi <markatcholi918@gmail.com>
2026-08-09 14:39:13 -0500

    Fix import path for supabaseClient in page.tsx
    Signed-off-by: Assih Atcholi <markatcholi918@gmail.com>

--- app/ncmrs/[id]/rework/page.tsx
+++ app/ncmrs/[id]/rework/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useMemo, useState } from "react";
 import Link from "next/link";
 import { useParams, useSearchParams } from "next/navigation";
-import { supabase } from "../../../lib/supabaseClient";
+import { supabase } from "../../../../lib/supabaseClient";
 
 export default function NcmrReworkWorkPackagePage() {
   const params = useParams<{ id: string }>();
